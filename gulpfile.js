657e3164fc4fcc41507299fa3559ab3e006b4cdc
sake811 <1.sake@bk.ru>
2021-11-27 12:17:09 +0300

    added search section

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,8 @@ let gulp = require('gulp'),
     cssmin = require('gulp-cssmin');
 
 gulp.task('sass', function(){
-   return gulp.src('app/scss/style.scss')
-           .pipe(sass({outputStyle: 'compressed'}))
+   return gulp.src('app/scss/**/*.scss')
+           .pipe(sass({outputStyle: 'expanded'}))
            .pipe(rename({suffix : '.min'}))
            .pipe(autoprefixer({
             overrideBrowserslist: ['last 8 versions']
@@ -61,10 +61,10 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function(){
-  gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
+  gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
   gulp.watch('app/*.html', gulp.parallel('html'))
   gulp.watch('app//js/*.js', gulp.parallel('js'));
 });
 
-gulp.task('default', gulp.parallel('style','script','sass','watch', 'browser-sync' ))
+gulp.task('default', gulp.parallel('style','script','sass','watch', 'browser-sync' ));
     
