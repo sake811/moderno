bfcb6d1ff07802709c09d97166263a0bc5b7ec8e
sake811 <1.sake@bk.ru>
2021-11-29 12:25:00 +0300

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,22 @@
-$(function(){
+$(function () {
+
+    var mixer = mixitup('.products__inner-box');
+
+
+    $('.rate-star').rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: "true"
+    });
+
+    $('.product__sldier-inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
+
+
 
 
 
